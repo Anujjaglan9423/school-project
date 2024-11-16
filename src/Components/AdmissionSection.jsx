c28a7d623f4e62d69fc38790947607fb9cae0794
Anujjaglan9423 <anujjaglan9423@gmail.com>
2024-11-16 19:54:00 +0530

    please change

--- src/Components/AdmissionSection.jsx
+++ src/Components/AdmissionSection.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
 
-const Section = tw.div`relative flex items-center justify-center h-[400px] bg-cover bg-center`;
+const Section = tw.div`relative flex  justify-center h-[400px] bg-cover bg-center`;
 const Overlay = tw.div`absolute inset-0 bg-black bg-opacity-30`;
-const Content = tw.div`relative text-center text-white p-6`;
-const Title = tw.h2`text-3xl font-bold mb-2`;
-const Info = tw.p`mb-1`;
+const Content = tw.div`relative text-center text-white py-32  px-6 bg-white bg-opacity-80 items-center`;
+const Title = tw.h2`text-3xl font-bold text-red-600 mb-2 uppercase`;
+const Info = tw.p`mb-1 text-gray-600`;
 
 
 const AdmissionSection = () => {
